c88d9d234d72b32996c6336475d27d61f0feb6a3
André Abadesso <andre.abadesso@gmail.com>
2014-07-22 21:37:59 -0300

    Factory para requests

--- app/js/services.js
+++ app/js/services.js
@@ -6,4 +6,26 @@
 // Demonstrate how to register services
 // In this case it is a simple value service.
 angular.module('myApp.services', []).
-  value('version', '0.1');
+value('version', '0.1')
+    .factory('Cors', function($http, $q) {
+        return {
+            get: function(url) {
+                return $http.get(base + url)
+                    .then(function(response) {
+                        // Adicionar lógica extra aqui
+                        return response.data;
+                    }, function(response) {
+                        return $q.reject(response.data);
+                    });
+            },
+            post: function(url, data) {
+                return $http.post(base + url, data)
+                    .then(function(response) {
+                        // Adicionar lógica extra aqui
+                        return response.data;
+                    }, function(response) {
+                        return $q.reject(response.data);
+                    });
+            }
+        }
+    });
