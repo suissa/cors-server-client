dcc37e239abc82e13b9c0d6d6edc566deca18ebe
Suissa <jnascimento@gmail.com>
2014-07-24 15:59:02 -0300

    refactoring

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,39 +3,39 @@
 /* Controllers */
 
 angular.module('myApp.controllers', [])
-    .controller('MyCtrl1', ['$scope', 'Cors',
-        function($scope, Cors) {
+  .controller('MyCtrl1', ['$scope', 'Cors',
+    function($scope, Cors) {
 
-            var base = 'http://cors-server.getup.io/url/';
-            var url = 'api.redtube.com/?data=redtube.Videos.searchVideos&search=Sasha%20Gray';
+      var base = 'http://cors-server.getup.io/url/';
+      var url = 'api.redtube.com/?data=redtube.Videos.searchVideos&search=Sasha%20Gray';
 
-            $scope.urlFull = base + url;
-            $scope.url = url;
+      $scope.urlFull = base + url;
+      $scope.url = url;
 
-            $scope.submit = function() {
-                find(this.url);
-            }
+      $scope.submit = function() {
+          find(this.url);
+      }
 
-            var find = function(url) {
-                $scope.loading = true;
-                Cors.get(url)
-                    .then(function(data) {
-                        $scope.data = data;
-                        $scope.loading = false;
-                        console.log('success', data);
-                    }, function(error) {
-                        console.log('error', data);
-                        $scope.loading = false;
-                    });
-            }
+      var find = function(url) {
+        $scope.loading = true;
+        Cors.get(url)
+          .then(function(data) {
+            $scope.data = data;
+            $scope.loading = false;
+            console.log('success', data);
+          }, function(error) {
+            console.log('error', data);
+            $scope.loading = false;
+          });
+      }
 
-            $scope.find = find;
-            find($scope.url);
+      $scope.find = find;
+      find($scope.url);
 
-        }
-    ])
-    .controller('MyCtrl2', ['$scope',
-        function($scope) {
+    }
+  ])
+  .controller('MyCtrl2', ['$scope',
+    function($scope) {
 
-        }
-    ]);
+    }
+  ]);
--- app/js/services.js
+++ app/js/services.js
@@ -7,25 +7,49 @@
 // In this case it is a simple value service.
 angular.module('myApp.services', []).
 value('version', '0.1')
-    .factory('Cors', function($http, $q) {
-        return {
-            get: function(url) {
-                return $http.get(base + url)
-                    .then(function(response) {
-                        // Adicionar lógica extra aqui
-                        return response.data;
-                    }, function(response) {
-                        return $q.reject(response.data);
-                    });
-            },
-            post: function(url, data) {
-                return $http.post(base + url, data)
-                    .then(function(response) {
-                        // Adicionar lógica extra aqui
-                        return response.data;
-                    }, function(response) {
-                        return $q.reject(response.data);
-                    });
-            }
-        }
-    });
+  .factory('Cors', function($http, $q) {
+    return {
+      get: function(url) {
+        return $http.get(base + url)
+          .then(function(response) {
+              // Adicionar lógica extra aqui
+              return response.data;
+          }, function(response) {
+              return $q.reject(response.data);
+          });
+      },
+      post: function(url, data) {
+        return $http.post(base + url, data)
+          .then(function(response) {
+              // Adicionar lógica extra aqui
+              return response.data;
+          }, function(response) {
+              return $q.reject(response.data);
+          });
+      }
+    }
+  }).
+  service('BeerService', ['$http',
+    function($http) {
+      var urlBase = 'api.openbeerdatabase.com/v1/beers.json';
+      this.find = function() {
+        return $http.get(urlBase);
+      };
+
+      this.findOne = function(id) {
+          return $http.get(urlBase + '/_id/' + id);
+      };
+
+      this.add = function(data) {
+          return $http.post(urlBase, data);
+      };
+
+      this.update = function(data) {
+          return $http.put(urlBase + '/_id/' + data._id, data)
+      };
+
+      this.remove = function(data) {
+          return $http.delete(urlBase + '/_id/' + data._id, data)
+      };
+    }
+  ]);
